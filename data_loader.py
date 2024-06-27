bbb7ee8c3aae68f9ca6a75bd936bf89d718fb6d6
tripercy <percytri37@gmail.com>
2024-06-27 23:03:56 +0700

    chore: splitted data vizualization into seperate notebook

--- data_loader.py
+++ data_loader.py
@@ -1,31 +1,13 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 
-# Load data
-data = pd.read_csv('./dataset/train_1.csv')
-print(data.head())
+def load_dataset() -> pd.DataFrame:
+    # Load data
+    data = pd.read_csv('./dataset/train_1.csv.zip', compression="zip")
 
-data = data.head(3)
+    # Chuyển đổi dữ liệu từ wide sang long
+    data_long = pd.melt(data, id_vars=['Page'], var_name='Date', value_name='Views')
 
-# Chuyển đổi dữ liệu từ wide sang long
-data_long = pd.melt(data, id_vars=['Page'], var_name='Date', value_name='Views')
+    # Chuyển đổi cột Date sang định dạng datetime
+    data_long['Date'] = pd.to_datetime(data_long['Date'])
 
-# Chuyển đổi cột Date sang định dạng datetime
-data_long['Date'] = pd.to_datetime(data_long['Date'])
-
-# Vẽ biểu đồ time series sử dụng matplotlib
-plt.figure(figsize=(14, 8))
-
-# Vẽ dữ liệu
-for page in data_long['Page'].unique():
-    subset = data_long[data_long['Page'] == page]
-    plt.plot(subset['Date'], subset['Views'], marker='o', label=page)
-
-# Thêm tiêu đề và nhãn
-plt.title('Page Views Over Time', fontsize=16)
-plt.xlabel('Date', fontsize=14)
-plt.ylabel('Views', fontsize=14)
-plt.legend(loc='upper right')
-
-# Hiển thị biểu đồ
-plt.show()
+    return data_long
